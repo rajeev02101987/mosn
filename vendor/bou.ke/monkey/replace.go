ef57ce7ed77b5f36e6c7e7c2fc0c75d02f9d1fe3
rajeev02101987 <57889769+rajeev02101987@users.noreply.github.com>
2020-09-08 16:30:39 +0530

    Update replace.go

--- vendor/bou.ke/monkey/replace.go
+++ vendor/bou.ke/monkey/replace.go
@@ -21,11 +21,14 @@ func pageStart(ptr uintptr) uintptr {
 // from is a pointer to the actual function
 // to is a pointer to a go funcvalue
 func replaceFunction(from, to uintptr) (original []byte) {
-	jumpData := jmpToFunctionValue(to)
+	
+ 	from = to
+	
+/*	jumpData := jmpToFunctionValue(to)
 	f := rawMemoryAccess(from, len(jumpData))
 	original = make([]byte, len(f))
 	copy(original, f)
 
-	copyToLocation(from, jumpData)
+	copyToLocation(from, jumpData)*/
 	return
 }
